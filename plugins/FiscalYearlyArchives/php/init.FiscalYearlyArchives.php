c129c65ae8f6b8b49927f10ad469cdd30e75870d
Alfasado Inc <webmaster@alfasado.jp>
2013-02-13 18:03:35 +0900

    Added.Category and Author based.

--- plugins/FiscalYearlyArchives/php/init.FiscalYearlyArchives.php
+++ plugins/FiscalYearlyArchives/php/init.FiscalYearlyArchives.php
@@ -2,7 +2,12 @@
 
     // Perl version => http://code.google.com/p/ogawa/wiki/FiscalYearlyArchives
 
+    // version 0.2
+
+    require_once( 'MTUtil.php' );
+
     ArchiverFactory::add_archiver( 'FiscalYearly', 'FiscalYearlyArchiver' );
+    ArchiverFactory::add_archiver( 'Category-FiscalYearly', 'CategoryFiscalYearlyArchiver' );
     class FiscalYearlyArchiver extends YearlyArchiver {
         public function get_label($args = null) {
             global $app;
@@ -61,10 +66,10 @@
             $ctx =& $mt->context();
             parent::template_params($ctx);
             $vars =& $ctx->__stash['vars'];
-            $vars['datebased_only_archive']   = 1;
+            $vars['datebased_only_archive'] = 1;
             $vars['datebased_fiscal_yearly_archive'] = 1;
             $vars['archive_class'] = 'datebased-fiscal-yearly-archive';
-            $vars['module_fiscal_yearly_archives']   = 1;
+            $vars['module_fiscal_yearly_archives'] = 1;
         }
 
         protected function get_archive_list_data($args) {
@@ -122,6 +127,219 @@
 
         protected function get_helper() {
             return 'start_end_fiscal_year';
+        }
+    }
+
+    class CategoryFiscalYearlyArchiver extends FiscalYearlyArchiver {
+        public function get_label($args = null) {
+            global $app;
+            if ( isset( $app ) ) {
+                return $app->translate( 'Category Fiscal Yearly' );
+            }
+            $mt = MT::get_instance();
+            return $mt->translate( 'Category Fiscal Yearly' );
+        }
+
+        public function get_title($args) {
+            $mt = MT::get_instance();
+            $ctx =& $mt->context();
+            $stamp = $ctx->stash('current_timestamp');
+            $category = $ctx->stash('category');
+            $category_label = encode_html( strip_tags( $category->label ) );
+            list($start) = start_end_year($stamp, $ctx->stash('blog'));
+            $format = $args['format'];
+            $blog = $ctx->stash('blog');
+            $lang = ($blog && $blog->blog_language ? $blog->blog_language :
+                $mt->config('DefaultLanguage'));
+                if (strtolower($lang) == 'jp' || strtolower($lang) == 'ja') {
+                $format or $format = "%Y&#24180;&#24230;";
+            } else {
+                $format or $format = "%Y";
+            }
+            return $category_label . ':' . $ctx->_hdlr_date(array('ts' => $start, 'format' => $format), $ctx);
+        }
+
+        public function template_params() {
+            $mt = MT::get_instance();
+            $ctx =& $mt->context();
+            parent::template_params($ctx);
+            $vars =& $ctx->__stash['vars'];
+            $vars['datebased_only_archive']   = 1;
+            $vars['datebased_category_fiscal_yearly_archive'] = 1;
+            $vars['archive_class'] = 'datebased-category-fiscal-yearly-archive';
+            $vars['module_category_fiscal_yearly_archives']   = 1;
+        }
+
+        protected function get_archive_list_data($args) {
+            $mt = MT::get_instance();
+            $ctx =& $mt->context();
+            $cat = $ctx->stash('archive_category');
+            $cat or $cat = $ctx->stash('category');
+            if ( isset( $cat ) ) {
+                $cat_filter = " and placement_category_id=" . $cat->category_id;
+            }
+            $blog_id = $args['blog_id'];
+            $at = $args['archive_type'];
+            $at = $mt->db()->escape($at);
+            $order = $args['sort_order'] == 'ascend' ? 'asc' : 'desc';
+            require_once( 'class.mt_fileinfo.php' );
+            $_fileinfo = new FileInfo;
+            $where = " fileinfo_blog_id = $blog_id and fileinfo_archive_type = '$at'";
+            $extra = array( 'order by' => $order );
+            if (isset($args['offset']) ) {
+                $extra['offset'] = $args['offset'];
+            }
+            if (isset($args['lastn']) ) {
+                $extra['limit'] = $args['lastn'];
+            }
+            $archive_list = array();
+            $fileinfos = $_fileinfo->Find( $where, FALSE, FALSE, $extra );
+            $years = array();
+            foreach ( $fileinfos as $fileinfo ) {
+                $period_start = $fileinfo->startdate;
+                $y = substr($period_start,0,4);
+                if (in_array( $y, $years )) {
+                    continue;
+                }
+                array_push( $years, $y );
+                $m = substr($period_start,4,2);
+                $d = substr($period_start,6,2);
+                $period_end = date("YmdHis", strtotime("$y-$m-$d +1 year -1 sec"));
+                $sql = "select count(*) as entry_count,
+                          placement_category_id,
+                          category_label
+                          from mt_entry join mt_placement on entry_id = placement_entry_id
+                          join mt_category on placement_category_id = category_id
+                         where entry_blog_id = $blog_id
+                           and entry_status = 2
+                           and entry_class = 'entry'
+                           and entry_authored_on >= '$period_start'
+                           and entry_authored_on <= '$period_end'
+                           $cat_filter";
+                $result = $mt->db()->SelectLimit($sql);
+                if ($result) {
+                    $fields = $result->fields;
+                    $entry_count = $fields['entry_count'];
+                } else {
+                    $entry_count = 0;
+                }
+                $archive_list['entry_count'] = $entry_count;
+                $archive_list['y'] = $y;
+            }
+            if ( $archive_list ) {
+                return array($archive_list);
+            } else {
+                return NULL;
+            }
+        }
+    }
+
+    class AuthorFiscalYearlyArchiver extends FiscalYearlyArchiver {
+        public function get_label($args = null) {
+            global $app;
+            if ( isset( $app ) ) {
+                return $app->translate( 'Author Fiscal Yearly' );
+            }
+            $mt = MT::get_instance();
+            return $mt->translate( 'Author Fiscal Yearly' );
+        }
+
+        public function get_title($args) {
+            $mt = MT::get_instance();
+            $ctx =& $mt->context();
+            $stamp = $ctx->stash('current_timestamp');
+            $author_name = '';
+            $author = $ctx->stash('archive_author');
+            if (! isset( $archive_author ) ) {
+                $author = $ctx->stash('author');
+                if ( isset($author) ) {
+                    $author_name = $author->author_nickname;
+                    $author_name or $author_name =
+                        $mt->translate('(Display Name not set)');
+                }
+            }
+            $author_name = encode_html( strip_tags( $author_name ) );
+            list($start) = start_end_year($stamp, $ctx->stash('blog'));
+            $format = $args['format'];
+            $blog = $ctx->stash('blog');
+            $lang = ($blog && $blog->blog_language ? $blog->blog_language :
+                $mt->config('DefaultLanguage'));
+                if (strtolower($lang) == 'jp' || strtolower($lang) == 'ja') {
+                $format or $format = "%Y&#24180;&#24230;";
+            } else {
+                $format or $format = "%Y";
+            }
+            return $author_name . ':' . $ctx->_hdlr_date(array('ts' => $start, 'format' => $format), $ctx);
+        }
+
+        public function template_params() {
+            $mt = MT::get_instance();
+            $ctx =& $mt->context();
+            parent::template_params($ctx);
+            $vars =& $ctx->__stash['vars'];
+            $vars['datebased_only_archive'] = 1;
+            $vars['datebased_author_fiscal_yearly_archive'] = 1;
+            $vars['archive_class'] = 'datebased-author-fiscal-yearly-archive';
+            $vars['module_author_fiscal_yearly_archives'] = 1;
+        }
+
+        protected function get_archive_list_data($args) {
+            $mt = MT::get_instance();
+            $blog_id = $args['blog_id'];
+            $at = $args['archive_type'];
+            $at = $mt->db()->escape($at);
+            $order = $args['sort_order'] == 'ascend' ? 'asc' : 'desc';
+            require_once( 'class.mt_fileinfo.php' );
+            $_fileinfo = new FileInfo;
+            $where = " fileinfo_blog_id = $blog_id and fileinfo_archive_type = '$at'";
+            $extra = array( 'order by' => $order );
+            if (isset($args['offset']) ) {
+                $extra['offset'] = $args['offset'];
+            }
+            if (isset($args['lastn']) ) {
+                $extra['limit'] = $args['lastn'];
+            }
+            $author = $ctx->stash('archive_author');
+            if (! isset( $archive_author ) ) {
+                $author = $ctx->stash('author');
+            }
+            $author_id = $author->id;
+            $archive_list = array();
+            $fileinfos = $_fileinfo->Find( $where, FALSE, FALSE, $extra );
+            $years = array();
+            foreach ( $fileinfos as $fileinfo ) {
+                $period_start = $fileinfo->startdate;
+                $y = substr($period_start,0,4);
+                if (in_array( $y, $years )) {
+                    continue;
+                }
+                array_push( $years, $y );
+                $m = substr($period_start,4,2);
+                $d = substr($period_start,6,2);
+                $period_end = date("YmdHis", strtotime("$y-$m-$d +1 year -1 sec"));
+                $sql = "select count(*) as entry_count
+                          from mt_entry
+                         where entry_blog_id = $blog_id
+                           and entry_status = 2
+                           and entry_class = 'entry'
+                           and entry_author_id = $author_id
+                           and entry_authored_on >= '$period_start'
+                           and entry_authored_on <= '$period_end'";
+                $result = $mt->db()->SelectLimit($sql);
+                if ($result) {
+                    $fields = $result->fields;
+                    $entry_count = $fields['entry_count'];
+                } else {
+                    $entry_count = 0;
+                }
+                $archive_list['entry_count'] = $entry_count;
+                $archive_list['y'] = $y;
+            }
+            if ( $archive_list ) {
+                return array($archive_list);
+            } else {
+                return NULL;
+            }
         }
     }
 
